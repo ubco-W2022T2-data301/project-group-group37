afd80ef19b3509b07f279b0d9c113ed207367023
darvie <darveshsingh2002@gmail.com>
2023-04-11 18:58:27 -0700

    Eddited final report

--- analysis/code/project_functions1.py
+++ analysis/code/project_functions1.py
@@ -1,3 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+
 def convert_age_to_int(x):
     """Converts the age range (x) to an integer.
 
